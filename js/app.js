79fac6cf2aae0343a5f1be56925373c0fff4e6fa
Tim <timyaohoyt80@gmail.com>
2017-06-18 01:32:35 -0400

    Adding warning signal

--- js/app.js
+++ js/app.js
@@ -192,8 +192,8 @@ $(document).ready(function () {
     </response_columns>
 </request>`;
 
-     function cuReady() {
-        var yearArray = ["2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027"];
+    function cuReady() {
+        var yearArray = ["2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"];
         var vendorCodeArray = ["VS00013433", "VC00168092", "0003387756", "0001196340"];
         for (var i = 0; i < yearArray.length; i++) {
             for (var j = 0; j < vendorCodeArray.length; j++) {
@@ -204,7 +204,7 @@ $(document).ready(function () {
                     var yearRange = parseInt(yearArray[i]);
                     var vendorCode = vendorCodeArray[j];
                     // console.log(vendorCode, yearRange);
-                    spendingRequest =`<request>
+                    spendingRequest = `<request>
     <type_of_data>Spending</type_of_data>
     <records_from>1</records_from>
     <max_records>1000</max_records>
@@ -241,52 +241,53 @@ $(document).ready(function () {
         <column>industry</column>        
     </response_columns>
 </request>`;
-            console.log("yes");
+                     spendingCityWideMode(spendingRequest);
+                    // console.log("yes");
                 }
 
             }
         }
     };
 
-//     function cuReadyPlugIn(vendorCode, yearRange) {
-//         var spendingRequest = `<request>
-//     <type_of_data>Spending</type_of_data>
-//     <records_from>1</records_from>
-//     <max_records>1000</max_records>
-//     <search_criteria>
-//         <criteria>
-//             <name>payee_code</name>
-//             <type>value</type>
-//             <value>${vendorCode}</value>
-//         </criteria>
-//         <criteria>
-//             <name>calendar_year</name>
-//             <type>value</type>
-//             <value>${yearRange}</value>
-//         </criteria>
-//     </search_criteria>
-//     <response_columns>
-//         <column>agency</column>
-//         <column>fiscal_year</column>
-//         <column>spending_category</column>
-//         <column>document_id</column>
-//         <column>payee_name</column>
-//         <column>check_amount</column>
-//         <column>department</column>
-//         <column>sub_vendor</column>
-//         <column>sub_contract_reference_id</column>
-//         <column>expense_category</column>
-//         <column>calendar_year</column>
-//         <column>contract_ID</column>
-//         <column>contract_purpose</column>
-//         <column>issue_date</column>
-//         <column>capital_project</column>
-//         <column>mwbe_category</column>
-//         <column>associated_prime_vendor</column>
-//         <column>industry</column>        
-//     </response_columns>
-// </request>`;
-//     };
+    //     function cuReadyPlugIn(vendorCode, yearRange) {
+    //         var spendingRequest = `<request>
+    //     <type_of_data>Spending</type_of_data>
+    //     <records_from>1</records_from>
+    //     <max_records>1000</max_records>
+    //     <search_criteria>
+    //         <criteria>
+    //             <name>payee_code</name>
+    //             <type>value</type>
+    //             <value>${vendorCode}</value>
+    //         </criteria>
+    //         <criteria>
+    //             <name>calendar_year</name>
+    //             <type>value</type>
+    //             <value>${yearRange}</value>
+    //         </criteria>
+    //     </search_criteria>
+    //     <response_columns>
+    //         <column>agency</column>
+    //         <column>fiscal_year</column>
+    //         <column>spending_category</column>
+    //         <column>document_id</column>
+    //         <column>payee_name</column>
+    //         <column>check_amount</column>
+    //         <column>department</column>
+    //         <column>sub_vendor</column>
+    //         <column>sub_contract_reference_id</column>
+    //         <column>expense_category</column>
+    //         <column>calendar_year</column>
+    //         <column>contract_ID</column>
+    //         <column>contract_purpose</column>
+    //         <column>issue_date</column>
+    //         <column>capital_project</column>
+    //         <column>mwbe_category</column>
+    //         <column>associated_prime_vendor</column>
+    //         <column>industry</column>        
+    //     </response_columns>
+    // </request>`;
+    //     };
 
 
 
@@ -348,7 +349,7 @@ $(document).ready(function () {
             console.log(90, flagBit);
             if (flagBit == "Budget") {
                 budgetMode(budgetRequestNow);
-                console.log("head",budgetRequestNow)
+                console.log("head", budgetRequestNow)
                 $("#TableTest").show();
                 $("#budgetView").show();
                 $("#contractsView").hide();
@@ -1105,11 +1106,11 @@ $(document).ready(function () {
             }
             else if (flagBit == "Spending-Citywide") {
                 cuReady();
-                console.log(990,vendorCode, yearRange);
-                //  console.log(99,vendorCode,yearRange);              
-                // cuReadyPlugIn(vendorCode, yearRange);
-                console.log(880,spendingRequest);
-                spendingCityWideMode(spendingRequest);
+                // console.log(990, vendorCode, yearRange);
+                // //  console.log(99,vendorCode,yearRange);              
+                // // cuReadyPlugIn(vendorCode, yearRange);
+                // console.log(880, spendingRequest);
+                // spendingCityWideMode(spendingRequest);
                 $("#TableTest").show();
                 $("#alertSignal").hide();
                 $("#budgetView").hide();
@@ -1287,47 +1288,57 @@ $(document).ready(function () {
                 cache: false,
                 error: function () { alert("No data found."); },
                 success: function (xml) {
-                    console.log(199,xml);
+                    console.log(199, xml);
                     var result = (new XMLSerializer()).serializeToString(xml);
                     xmlDoc = $.parseXML(result),
                         $xml = $(xmlDoc),
                         $response = $xml.find("response");
-                        var spendingFlag = $response.find("code").text();
-                        console.log(1296,spendingFlag);
-                        if(spendingFlag==1){
-                            console.log("No data!");
+                    var spendingFlag = $response.find("code").text();
+                    var countFlag = parseInt($response.find("record_count").text());
+                    var countFlagProcessed = (countFlag/1000)+1;
+                    console.log(1296, countFlagProcessed);
+                        if(countFlagProcessed<2){
+                            console.log("one time");
+                        }else if(2<countFlagProcessed<3){
+                           console.log("two time");
+                        }else if(3<countFlagProcessed<4){
+                             console.log("three time");
+                        }else if(4<countFlagProcessed<5){
+                            console.log("three time");
                         }
-                    $response.find("transaction").each(function () {
-                        var agency = $(this).find("agency").text();
-                        var aptPin = $(this).find("associated_prime_vendor").text();
-                        var department = $(this).find("calendar_year").text();
-                        // var expenseCategory = $(this).find("award_method").text();
-                        var budgetCode = $(this).find("contract_ID").text();
-                        var budgetNode = $(this).find("contract_purpose").text();
-                        var modified = $(this).find("check_amount").text();
-                        var adopted = $(this).find("department").text();
-                        var preEncumbered = $(this).find("document_id").text();
-                        var encumbered = $(this).find("expense_category").text();
-                        var cashExpense = $(this).find("fiscal_year").text();
-                        var postAdjustment = $(this).find("industry").text();
-                        var accruedExpense = $(this).find("issue_date").text();
-                        var pci = $(this).find("mwbe_category").text();
-                        var pin = $(this).find("payee_name").text();
-                        var purpose = $(this).find("spending_category").text();
-                        var rd = $(this).find("sub_vendor").text();
-                        var std = $(this).find("spent_to_date").text();
-                        var sb = $(this).find("sub_vendor").text();
-                        // var vendor = $(this).find("vendor").text();
-                        // var version = $(this).find("version").text();
-                        // var year = $(this).find("year").text();
+                           
+                    // if (spendingFlag == 1) {
+                    //     dataFlagViewerTrigger();
+                    // } else {
+                        $response.find("transaction").each(function () {
+                            var agency = $(this).find("agency").text();
+                            var aptPin = $(this).find("associated_prime_vendor").text();
+                            var department = $(this).find("calendar_year").text();
+                            // var expenseCategory = $(this).find("award_method").text();
+                            var budgetCode = $(this).find("contract_ID").text();
+                            var budgetNode = $(this).find("contract_purpose").text();
+                            var modified = $(this).find("check_amount").text();
+                            var adopted = $(this).find("department").text();
+                            var preEncumbered = $(this).find("document_id").text();
+                            var encumbered = $(this).find("expense_category").text();
+                            var cashExpense = $(this).find("fiscal_year").text();
+                            var postAdjustment = $(this).find("industry").text();
+                            var accruedExpense = $(this).find("issue_date").text();
+                            var pci = $(this).find("mwbe_category").text();
+                            var pin = $(this).find("payee_name").text();
+                            var purpose = $(this).find("spending_category").text();
+                            var rd = $(this).find("sub_vendor").text();
+                            var std = $(this).find("spent_to_date").text();
+                            var sb = $(this).find("sub_vendor").text();                           
 
-                        var tr = $("<tr/>");
-                        tr.append("<td>" + agency + "</td>" + "<td>" + aptPin + "</td>" + "<td>" + department + "</td>" + "<td>" + budgetCode + "</td>" + "<td>" + budgetNode + "</td>" + "<td>" + modified + "</td>" + "<td>" + adopted + "</td>" + "<td>"
-                            + preEncumbered + "</td>" + "<td>" + encumbered + "</td>" + "<td>" + cashExpense + "</td>" + "<td>" + postAdjustment + "</td>"
-                            + "<td>" + accruedExpense + "</td>" + "<td>" + pci + "</td>" + "<td>" + pin + "</td>" + "<td>" + purpose + "</td>" + "<td>" + rd + "</td>"
-                            + "<td>" + std + "</td>" + "<td>" + sb + "</td>");
-                        $("#TableTest").append(tr);
-                    });
+                            var tr = $("<tr/>");
+                            tr.append("<td>" + agency + "</td>" + "<td>" + aptPin + "</td>" + "<td>" + department + "</td>" + "<td>" + budgetCode + "</td>" + "<td>" + budgetNode + "</td>" + "<td>" + modified + "</td>" + "<td>" + adopted + "</td>" + "<td>"
+                                + preEncumbered + "</td>" + "<td>" + encumbered + "</td>" + "<td>" + cashExpense + "</td>" + "<td>" + postAdjustment + "</td>"
+                                + "<td>" + accruedExpense + "</td>" + "<td>" + pci + "</td>" + "<td>" + pin + "</td>" + "<td>" + purpose + "</td>" + "<td>" + rd + "</td>"
+                                + "<td>" + std + "</td>" + "<td>" + sb + "</td>");
+                            $("#TableTest").append(tr);
+                        });
+                    // };
                 }
             });
         });
@@ -1363,5 +1374,10 @@ $(document).ready(function () {
             worksheetName: ['Table 1', 'Table 2', 'Table 3']
         });
     };
+
+    function dataFlagViewerTrigger() {
+        document.getElementById("dataFlagViewer").innerHTML = "No Data Found in this parameter! Please reload page and try again!";
+    };
+
 
 });
