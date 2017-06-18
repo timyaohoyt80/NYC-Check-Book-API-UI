dff1109f51b23010daee92e8325f36a74739c67f
Tim <timyaohoyt80@gmail.com>
2017-06-18 17:31:47 -0400

    Add spending-other function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var a = 2013;
     var b = 2;
     var vendorCode;
-    var yearRange;    
+    var yearRange;
     var budgetRequestNow = `<request>
     <type_of_data>Budget</type_of_data>
     <max_records>50</max_records>
@@ -195,9 +195,9 @@ $(document).ready(function () {
             for (var j = 0; j < vendorCodeArray.length; j++) {
                 if (yearArray[i] == vendorCodeArray[j]) {
                     console.log('error!');
-                } else {                  
+                } else {
                     var yearRange = parseInt(yearArray[i]);
-                    var vendorCode = vendorCodeArray[j];                    
+                    var vendorCode = vendorCodeArray[j];
                     spendingRequest = `<request>
     <type_of_data>Spending</type_of_data>
     <records_from>1</records_from>
@@ -235,12 +235,63 @@ $(document).ready(function () {
         <column>industry</column>        
     </response_columns>
 </request>`;
-                    spendingCityWideMode(spendingRequest);                
+                    spendingCityWideMode(spendingRequest);
                 }
 
             }
         }
-    };  
+    };
+
+    function spendingOtherThanCity() {
+        var yearArray = ["2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"];
+        var vendorCodeArray = ["manpowergroup public sector, inc", "manpowergroup us inc", "experis us inc", "comsys information technology services, llc"];
+        for (var i = 0; i < yearArray.length; i++) {
+            for (var j = 0; j < vendorCodeArray.length; j++) {
+                if (yearArray[i] == vendorCodeArray[j]) {
+                    console.log('error!');
+                } else {
+                    var yearRange = parseInt(yearArray[i]);
+                    var vendorCode = vendorCodeArray[j];
+                    spendingRequest = `<request>
+    <type_of_data>Spending_OGE</type_of_data>
+    <records_from>1</records_from>
+    <max_records>1000</max_records>
+    <search_criteria>
+        <criteria>
+            <name>payee_name</name>
+            <type>value</type>
+            <value>${vendorCode}</value>
+        </criteria>
+        <criteria>
+            <name>calendar_year</name>
+            <type>value</type>
+            <value>${yearRange}</value>
+        </criteria>
+    </search_criteria>
+    <response_columns>
+        <column>other_government_entities</column>      
+        <column>spending_category</column>
+        <column>check_amount</column>
+          <column>department</column>
+          <column>commodity_line</column>
+           <column>expense_category</column>
+              <column>calendar_year</column>              
+        <column>contract_id</column>
+        <column>contract_purpose</column>
+        <column>capital_project</column>
+         <column>budget_name</column>
+          <column>entity_contract_number</column>
+         <column>fiscal_year</column>
+        <column>payee_name</column>    
+              
+    </response_columns>
+</request>`;
+                    spendingOtherEntitiesMode(spendingRequest);
+                }
+
+            }
+        }
+    };
 
 
     $("#TableTest").hide();
@@ -365,7 +416,7 @@ $(document).ready(function () {
                 outputFlagBit();
 
             }
-            else if (flagBit == "Contracts-(Citywide) with Active and Registered status Category-Expense") {              
+            else if (flagBit == "Contracts-(Citywide) with Active and Registered status Category-Expense") {
                 contractsMode(contractsRequestNow);
                 $("#TableTest").show();
                 $("#alertSignal").hide();
@@ -1006,20 +1057,27 @@ $(document).ready(function () {
                 document.getElementById("alertSignal").innerHTML = "No Matched Data Found!";
             }
             else if (flagBit == "Spending-Citywide") {
-                cuReady();                
+                cuReady();
                 $("#TableTest").show();
                 $("#alertSignal").hide();
                 $("#budgetView").hide();
                 $("#contractsView").hide();
                 $("#contractsViewPending").hide();
                 $("#contractsViewOther").hide();
-                $("#spendingCityWideView").show();             
+                $("#spendingCityWideView").show();
                 outputFlagBit();
             }
             else if (flagBit == "Spending-Other Government Entities") {
-                $("#TableTest").hide();
-                $("#alertSignal").show();
-                document.getElementById("alertSignal").innerHTML = "No Matched Data Found!";
+                spendingOtherThanCity();
+                $("#TableTest").show();
+                 $("#alertSignal").hide();
+                $("#budgetView").hide();
+                $("#contractsView").hide();
+                $("#contractsViewPending").hide();
+                $("#contractsViewOther").hide();
+                $("#spendingCityWideView").hide();
+                $("#spendingOtherView").show();
+                 outputFlagBit();               
             }
         });
     });
@@ -1173,6 +1231,8 @@ $(document).ready(function () {
         outputFlagBit();
     };
 
+
+    //for cityWide-Spending
     function spendingCityWideMode(spendingRequest) {
         var boot = function () {
             $.ajax({
@@ -1231,7 +1291,7 @@ $(document).ready(function () {
                             + "<td>" + accruedExpense + "</td>" + "<td>" + pci + "</td>" + "<td>" + pin + "</td>" + "<td>" + purpose + "</td>" + "<td>" + rd + "</td>"
                             + "<td>" + std + "</td>" + "<td>" + sb + "</td>");
                         $("#TableTest").append(tr);
-                    });                  
+                    });
                 }
             });
         };
@@ -1283,7 +1343,112 @@ $(document).ready(function () {
                 }
             });
         };
-        boot();    
+        boot();
+    };
+
+    //for Other Governement Entities-spending
+        function spendingOtherEntitiesMode(spendingRequest) {
+        var boot = function () {
+            $.ajax({
+                type: "POST",
+                url: "http://www.checkbooknyc.com/api",
+                data: `${spendingRequest}`,
+                contentType: "text/xml",
+                crossDomain: true,
+                dataType: "xml",
+                cache: false,
+                error: function () { alert("No data found."); },
+                success: function (xml) {
+                    var result = (new XMLSerializer()).serializeToString(xml);
+                    xmlDoc = $.parseXML(result),
+                        $xml = $(xmlDoc),
+                        $response = $xml.find("response");
+                    var countFlag = parseInt($response.find("record_count").text());
+                    var countFlagProcessed = (countFlag / 1000) + 1;
+                    if (2 < countFlagProcessed < 3) {
+                        spendingRequestMini = spendingRequest.replace(/<records_from>[\s\S]*?<\/records_from>/, '<records_from>' + 1001 + '<\/records_from>');
+                        spendingCityWideModeMini(spendingRequestMini);
+                    } else if (3 < countFlagProcessed < 4) {
+                        spendingRequestMini = spendingRequest.replace(/<records_from>[\s\S]*?<\/records_from>/, '<records_from>' + 2001 + '<\/records_from>');
+                        spendingCityWideModeMini(spendingRequestMini);
+                    } else if (4 < countFlagProcessed < 5) {
+                        spendingRequestMini = spendingRequest.replace(/<records_from>[\s\S]*?<\/records_from>/, '<records_from>' + 3001 + '<\/records_from>');
+                        spendingCityWideModeMini(spendingRequestMini);
+                    } else if (5 < countFlagProcessed < 6) {
+                        spendingRequestMini = spendingRequest.replace(/<records_from>[\s\S]*?<\/records_from>/, '<records_from>' + 4001 + '<\/records_from>');
+                        spendingCityWideModeMini(spendingRequestMini);
+                    }
+
+                    $response.find("transaction").each(function () {
+                        var agency = $(this).find("other_government_entities").text();
+                        var aptPin = $(this).find("spending_category").text();
+                        var department = $(this).find("check_amount").text();
+                        var budgetCode = $(this).find("department").text();
+                        var budgetNode = $(this).find("commodity_line").text();
+                        var modified = $(this).find("expense_category").text();
+                        var adopted = $(this).find("calendar_year").text();
+                        var preEncumbered = $(this).find("contract_id").text();
+                        var encumbered = $(this).find("contract_purpose").text();
+                        var cashExpense = $(this).find("capital_project").text();
+                        var postAdjustment = $(this).find("budget_name").text();
+                        var accruedExpense = $(this).find("entity_contract_number").text();
+                        var pci = $(this).find("fiscal_year").text();
+                        var pin = $(this).find("payee_name").text();                    
+
+                        var tr = $("<tr/>");
+                        tr.append("<td>" + agency + "</td>" + "<td>" + aptPin + "</td>" + "<td>" + department + "</td>" + "<td>" + budgetCode + "</td>" + "<td>" + budgetNode + "</td>" + "<td>" + modified + "</td>" + "<td>" + adopted + "</td>" + "<td>"
+                            + preEncumbered + "</td>" + "<td>" + encumbered + "</td>" + "<td>" + cashExpense + "</td>" + "<td>" + postAdjustment + "</td>"
+                            + "<td>" + accruedExpense + "</td>" + "<td>" + pci + "</td>" + "<td>" + pin + "</td>");
+                        $("#TableTest").append(tr);
+                    });
+                }
+            });
+        };
+        boot();
+    };
+
+    function spendingCityWideModeMini(spendingRequestMini) {
+        var boot = function () {
+            $.ajax({
+                type: "POST",
+                url: "http://www.checkbooknyc.com/api",
+                data: `${spendingRequestMini}`,
+                contentType: "text/xml",
+                crossDomain: true,
+                dataType: "xml",
+                cache: false,
+                error: function () { alert("No data found."); },
+                success: function (xml) {
+                    var result = (new XMLSerializer()).serializeToString(xml);
+                    xmlDoc = $.parseXML(result),
+                        $xml = $(xmlDoc),
+                        $response = $xml.find("response");
+                    $response.find("transaction").each(function () {
+                        var agency = $(this).find("other_government_entities").text();
+                        var aptPin = $(this).find("spending_category").text();
+                        var department = $(this).find("check_amount").text();
+                        var budgetCode = $(this).find("department").text();
+                        var budgetNode = $(this).find("commodity_line").text();
+                        var modified = $(this).find("expense_category").text();
+                        var adopted = $(this).find("calendar_year").text();
+                        var preEncumbered = $(this).find("contract_id").text();
+                        var encumbered = $(this).find("contract_purpose").text();
+                        var cashExpense = $(this).find("capital_project").text();
+                        var postAdjustment = $(this).find("budget_name").text();
+                        var accruedExpense = $(this).find("entity_contract_number").text();
+                        var pci = $(this).find("fiscal_year").text();
+                        var pin = $(this).find("payee_name").text();
+
+                        var tr = $("<tr/>");
+                        tr.append("<td>" + agency + "</td>" + "<td>" + aptPin + "</td>" + "<td>" + department + "</td>" + "<td>" + budgetCode + "</td>" + "<td>" + budgetNode + "</td>" + "<td>" + modified + "</td>" + "<td>" + adopted + "</td>" + "<td>"
+                            + preEncumbered + "</td>" + "<td>" + encumbered + "</td>" + "<td>" + cashExpense + "</td>" + "<td>" + postAdjustment + "</td>"
+                            + "<td>" + accruedExpense + "</td>" + "<td>" + pci + "</td>" + "<td>" + pin + "</td>");
+                        $("#TableTest").append(tr);
+                    });
+                }
+            });
+        };
+        boot();
     };
 
     var outputFlagBit = function outputFlagBit() {
@@ -1291,11 +1456,11 @@ $(document).ready(function () {
             $(function () {
                 var outputNameFlag = $('#fileoutputname').val();
                 var outputFileType = $('input[name=outputTypeRadio]:checked').val();
-                if (outputFileType == null && outputNameFlag != null && outputNameFlag != "") {                  
+                if (outputFileType == null && outputNameFlag != null && outputNameFlag != "") {
                     outputCsv(outputNameFlag);
                 } else if (outputFileType == "xml" && outputNameFlag != null && outputNameFlag != "") {
                     outputXml(outputNameFlag);
-                } else if (outputFileType == "csv" && outputNameFlag != null && outputNameFlag != "") {                  
+                } else if (outputFileType == "csv" && outputNameFlag != null && outputNameFlag != "") {
                     outputCsv(outputNameFlag);
                 }
             });
